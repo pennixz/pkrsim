151aec87b0971c03a3c7f70a236dccbc38660b4e
Espen <espenlillejord@gmail.com>
2018-11-22 02:45:51 +0100

    fx

--- pokerhandsim.py
+++ pokerhandsim.py
@@ -1,6 +1,5 @@
 from deck import *
 
-
 d = Deck()
 table = Table(d)
 table.deck.create_cards()
@@ -9,8 +8,7 @@ table.flop()
 table.turn()
 table.river()
 
-
-#print(table.eval_equals(table.seat1.hand))
-#print(table.show_board())
+# print(table.eval_equals(table.seat1.hand))
+# print(table.show_board())
 print(table.seat1.get_hand())
 print(table.eval_straight(table.seat1.hand))
