fb6ad164ec19fc14a0003a37f47347aedab43d21
Espen <espenlillejord@gmail.com>
2018-11-24 18:22:09 +0100

    'updates'

--- deck.py
+++ deck.py
@@ -54,7 +54,7 @@ class Deck:
         # draw two cards from deck to seat.hand
         seat.hand.append(self.deck.pop(0))
         seat.hand.append(self.deck.pop(0))
-    
+
     def burn_card(self):
         self.burn_cards.append(self.deck.pop(0))
 
@@ -115,7 +115,7 @@ class Table:
         return res
 
     def flop(self):
-        
+
         self.deck.burn_card()
         for i in range(3):
             self.board.append(self.deck.draw_card())
@@ -125,9 +125,8 @@ class Table:
         self.board.append(self.deck.draw_card())
 
     def river(self):
-        self.deck.burn_card() 
+        self.deck.burn_card()
         self.board.append(self.deck.draw_card())
- 
 
     def eval_all(self, hand):
         self.eval_straight(hand)
@@ -139,7 +138,7 @@ class Table:
         # check for sorted numeric ranks to check straight(highest first)
         # todo: add check to see if an Ace makes a straight with 2345 if no other found
         # check for straight flush 
-        
+
         tmp = []
 
         for card in self.board:
@@ -150,7 +149,7 @@ class Table:
 
         tmp.sort(key=lambda y: y.rank[1])
         tmp.reverse()
-        
+
         for x in range(3):
 
             if (tmp[x].rank[1] == tmp[x + 1].rank[1] + 1
@@ -161,27 +160,30 @@ class Table:
                     and tmp[x + 2].suit == tmp[x + 3].suit
                     and tmp[x + 3].rank[1] == tmp[x + 4].rank[1] + 1
                     and tmp[x + 3].suit == tmp[x + 4].suit):
-                            if tmp[x].rank[1] == 14:
-                                print('ROYAL FUCKING FLUSH')
-                                print('{}{} {}{} {}{} {}{} {}{}'.format(tmp[x].rank[0], tmp[x].suit,
-                                    tmp[x + 1].rank[0], tmp[x + 1].suit, tmp[x + 2].rank[0], tmp[x + 2].suit, 
-                                    tmp[x + 3].rank[0], tmp[x + 3].suit, tmp[x + 4].rank[0], tmp[x + 4].suit))
-                            else:
-                                print('STRAIGHT FLUSH!')
-                                print('{}{} {}{} {}{} {}{} {}{}'.format(tmp[x].rank[0], tmp[x].suit,
-                                    tmp[x + 1].rank[0], tmp[x + 1].suit, tmp[x + 2].rank[0], tmp[x + 2].suit, 
-                                    tmp[x + 3].rank[0], tmp[x + 3].suit, tmp[x + 4].rank[0], tmp[x + 4].suit))
+                if tmp[x].rank[1] == 14:
+                    print('ROYAL FUCKING FLUSH')
+                    print('{}{} {}{} {}{} {}{} {}{}'.format(tmp[x].rank[0], tmp[x].suit,
+                                                            tmp[x + 1].rank[0], tmp[x + 1].suit, tmp[x + 2].rank[0],
+                                                            tmp[x + 2].suit,
+                                                            tmp[x + 3].rank[0], tmp[x + 3].suit, tmp[x + 4].rank[0],
+                                                            tmp[x + 4].suit))
+                else:
+                    print('STRAIGHT FLUSH!')
+                    print('{}{} {}{} {}{} {}{} {}{}'.format(tmp[x].rank[0], tmp[x].suit,
+                                                            tmp[x + 1].rank[0], tmp[x + 1].suit, tmp[x + 2].rank[0],
+                                                            tmp[x + 2].suit,
+                                                            tmp[x + 3].rank[0], tmp[x + 3].suit, tmp[x + 4].rank[0],
+                                                            tmp[x + 4].suit))
             elif (tmp[x].rank[1] == tmp[x + 1].rank[1] + 1
-                and tmp[x + 1].rank[1] == tmp[x + 2].rank[1] + 1
-                and tmp[x + 2].rank[1] == tmp[x + 3].rank[1] + 1
-                and tmp[x + 3].rank[1] == tmp[x + 4].rank[1] + 1):
+                  and tmp[x + 1].rank[1] == tmp[x + 2].rank[1] + 1
+                  and tmp[x + 2].rank[1] == tmp[x + 3].rank[1] + 1
+                  and tmp[x + 3].rank[1] == tmp[x + 4].rank[1] + 1):
                 print('STRAIGHT FOUND:')
                 format_it = '{}{} {}{} {}{} {}{} {}{}'.format(tmp[x].rank[0], tmp[x].suit, tmp[x + 1].rank[0],
                                                               tmp[x + 1].suit,
                                                               tmp[x + 2].rank[0], tmp[x + 2].suit, tmp[x + 3].rank[0],
                                                               tmp[x + 3].suit, tmp[x + 4].rank[0], tmp[x + 4].suit)
                 print(format_it)
-    
 
     def eval_flush(self, hand):
         hearts = []
@@ -204,9 +206,33 @@ class Table:
             hearts.sort(key=lambda y: y.rank[1])
             hearts.reverse()
             print('{}{} {}{} {}{} {}{} {}{}'.format(hearts[0].rank[0], hearts[0].suit, hearts[1].rank[0],
-                                                hearts[1].suit, hearts[2].rank[0], hearts[2].suit, hearts[3].rank[1],
-                                                hearts[3].suit, hearts[4].rank[0], hearts[4].suit))
-    
+                                                    hearts[1].suit, hearts[2].rank[0], hearts[2].suit,
+                                                    hearts[3].rank[1],
+                                                    hearts[3].suit, hearts[4].rank[0], hearts[4].suit))
+        if len(diamonds) >= 5:
+            print('FLUSH!')
+            diamonds.sort(key=lambda y: y.rank[1])
+            diamonds.reverse()
+            print('{}{} {}{} {}{} {}{} {}{}'.format(diamonds[0].rank[0],    diamonds[0].suit,   diamonds[1].rank[0],
+                                                    diamonds[1].suit,   diamonds[2].rank[0],    diamonds[2].suit,
+                                                    diamonds[3].rank[1],
+                                                    diamonds[3].suit,   diamonds[4].rank[0],    diamonds[4].suit))
+        if len(clubs) >= 5:
+            print('FLUSH!')
+            clubs.sort(key=lambda y: y.rank[1])
+            clubs.reverse()
+            print('{}{} {}{} {}{} {}{} {}{}'.format(clubs[0].rank[0], clubs[0].suit, clubs[1].rank[0],
+                                                    clubs[1].suit, clubs[2].rank[0], clubs[2].suit,
+                                                    clubs[3].rank[1],
+                                                    clubs[3].suit, clubs[4].rank[0], clubs[4].suit))
+        if len(spades) >= 5:
+            print('FLUSH!')
+            spades.sort(key=lambda y: y.rank[1])
+            spades.reverse()
+            print('{}{} {}{} {}{} {}{} {}{}'.format(spades[0].rank[0], spades[0].suit, spades[1].rank[0],
+                                                    spades[1].suit, spades[2].rank[0], spades[2].suit,
+                                                    spades[3].rank[1],
+                                                    spades[3].suit, spades[4].rank[0], spades[4].suit))
 
     def eval_equals(self, hand):
 
@@ -227,20 +253,20 @@ class Table:
             tem = []
             for j in range(hi):
                 if j == 0:
-                    if all_cards[i].rank[1] == all_cards[j+1].rank[1]:
+                    if all_cards[i].rank[1] == all_cards[j + 1].rank[1]:
                         tem.append(all_cards[i])
                 if all_cards[i].rank[1] == all_cards[j].rank[1]:
                     try:
                         if tem.index(all_cards[i]):
                             pass
-                    
+
                     except ValueError:
                         tem.append(all_cards[i])
-                    
+
                     try:
                         if tem.index(all_cards[j]):
                             pass
-                    
+
                     except ValueError:
                         tem.append(all_cards[j])
 
@@ -262,15 +288,15 @@ class Table:
         elif trips and dubs:
             if len(dubs) > 1:
                 top_pair = get_biggest_pair(dubs)
-            
-            print('FULL HOUSE with these cards: {}{} {}{} {}{} {}{} {}{}'.format(trips[0][0].rank[0], trips[0][0].suit, 
-                                                                    trips[0][1].rank[0], trips[0][1].suit,
-                                                                    trips[0][2].rank[0], trips[0][2].suit,
-                                                                    top_pair[0].rank[0], top_pair[0].suit,
-                                                                    top_pair[1].rank[0], top_pair[1].suit))
-                                                                   
+
+            print('FULL HOUSE with these cards: {}{} {}{} {}{} {}{} {}{}'.format(trips[0][0].rank[0], trips[0][0].suit,
+                                                                                 trips[0][1].rank[0], trips[0][1].suit,
+                                                                                 trips[0][2].rank[0], trips[0][2].suit,
+                                                                                 top_pair[0].rank[0], top_pair[0].suit,
+                                                                                 top_pair[1].rank[0], top_pair[1].suit))
+
         elif trips:
-            
+
             print('TRIPS with these cards: {}{} {}{} {}{}'.format(trips[0][0].rank[0], trips[0][0].suit,
                                                                   trips[0][1].rank[0],
                                                                   trips[0][1].suit, trips[0][2].rank[0],
@@ -286,6 +312,7 @@ class Table:
         else:
             print('High card: {}{}'.format(all_cards[0].rank[0], all_cards[0].suit))
 
+
 def get_biggest_pair(list_of_equals):
     ceil = 0
     top_pair = None
@@ -294,5 +321,5 @@ def get_biggest_pair(list_of_equals):
         if list_of_equals[x][0].rank[1] > ceil:
             ceil = list_of_equals[x][0].rank[1]
             top_pair = [list_of_equals[x][0], list_of_equals[x][1]]
-    
+
     return top_pair
--- pokerhandsim.py
+++ pokerhandsim.py
@@ -15,33 +15,41 @@ table.deck.draw_hand(table.seat6)
 table.flop()
 table.turn()
 table.river()
-#table.board = table.deck.create_royal_flush()
+# table.board = table.deck.create_royal_flush()
 
-print('Seat1 evals:')    
+print('Seat1 evals:')
+print('Hand: {}{} {}{}'.format(table.seat1.hand[0].rank[0], table.seat1.hand[0].suit, table.seat1.hand[1].rank[0],
+                               table.seat1.hand[1].suit))
 table.eval_straight(table.seat1.hand)
 table.eval_flush(table.seat1.hand)
 table.eval_equals(table.seat1.hand)
 print('Seat2 evals:')
+print('Hand: {}{} {}{}'.format(table.seat2.hand[0].rank[0], table.seat2.hand[0].suit, table.seat2.hand[1].rank[0],
+                               table.seat2.hand[1].suit))
 table.eval_straight(table.seat2.hand)
 table.eval_flush(table.seat2.hand)
 table.eval_equals(table.seat2.hand)
 print('Seat3 evals:')
+print('Hand: {}{} {}{}'.format(table.seat3.hand[0].rank[0], table.seat3.hand[0].suit, table.seat3.hand[1].rank[0],
+                               table.seat3.hand[1].suit))
 table.eval_straight(table.seat3.hand)
 table.eval_flush(table.seat3.hand)
 table.eval_equals(table.seat3.hand)
 print('Seat4 evals:')
+print('Hand: {}{} {}{}'.format(table.seat4.hand[0].rank[0], table.seat4.hand[0].suit, table.seat4.hand[1].rank[0],
+                               table.seat4.hand[1].suit))
 table.eval_straight(table.seat4.hand)
 table.eval_flush(table.seat4.hand)
 table.eval_equals(table.seat4.hand)
 print('Seat5 evals:')
+print('Hand: {}{} {}{}'.format(table.seat5.hand[0].rank[0], table.seat5.hand[0].suit, table.seat5.hand[1].rank[0],
+                               table.seat5.hand[1].suit))
 table.eval_straight(table.seat5.hand)
 table.eval_flush(table.seat5.hand)
 table.eval_equals(table.seat5.hand)
 print('Seat6 evals:')
+print('Hand: {}{} {}{}'.format(table.seat6.hand[0].rank[0], table.seat6.hand[0].suit, table.seat6.hand[1].rank[0],
+                               table.seat6.hand[1].suit))
 table.eval_straight(table.seat6.hand)
 table.eval_flush(table.seat6.hand)
 table.eval_equals(table.seat6.hand)
-
-
-
-
